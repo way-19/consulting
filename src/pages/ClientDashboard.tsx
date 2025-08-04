cd9543c5aac9b5e7add367242fd03b67bba1785e
way-19 <asparossystem@gmail.com>
2025-08-04 11:32:35 +0300

    Fix Supabase query ambiguity error

--- src/pages/ClientDashboard.tsx
+++ src/pages/ClientDashboard.tsx
@@ -721,7 +721,7 @@ const RecommendedServices: React.FC<{ clientId: string }> = ({ clientId }) => {
           *,
           consultant:users!service_payment_requests_consultant_id_fkey(
             first_name, last_name,
-            countries(name, flag_emoji)
+            country_id:countries(name, flag_emoji)
           ),
           recommended_service:consultant_custom_services(
             service_name, service_description, service_category
