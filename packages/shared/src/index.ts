26be58c25d46333fd3077e911eed023f4f82250d
way-19 <asparossystem@gmail.com>
2025-08-27 08:21:27 -0700

    Updated index.ts

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,4 @@
 // Shared components and utilities will be exported from here
 // This is a placeholder that will be populated when we move shared code
 
-export * from './contexts/AuthContext';
-export * from './contexts/LanguageContext';
+export {};
