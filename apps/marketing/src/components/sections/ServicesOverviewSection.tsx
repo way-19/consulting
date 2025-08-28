3386f38d82c92b002d22508f8257604636f16a2b
way-19 <asparossystem@gmail.com>
2025-08-28 03:33:50 -0700

    Updated ServicesOverviewSection.tsx

--- apps/marketing/src/components/sections/ServicesOverviewSection.tsx
+++ apps/marketing/src/components/sections/ServicesOverviewSection.tsx
@@ -83,7 +83,7 @@ const ServicesOverviewSection = () => {
   };
 
   return (
-    <section className="py-20 bg-white">
+    <section className="py-10 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16 relative">
           {/* Modern Header Background */}
