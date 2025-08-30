e78ca74bf49f5f8198c1a2bbd61249953d934b32
way-19 <asparossystem@gmail.com>
2025-08-30 10:39:19 -0700

    Fix port 5174 conflict

--- apps/client-dashboard/vite.config.ts
+++ apps/client-dashboard/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
+    port: 5177,
     host: true,
     strictPort: true,
   },
