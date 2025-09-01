7d2e31db2fbb45f661af109763a9ad12777eb11a
way-19 <asparossystem@gmail.com>
2025-09-01 02:57:44 -0700

    Updated vite.config.ts

--- apps/marketing/vite.config.ts
+++ apps/marketing/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { dirname, resolve } from 'path';
 import { fileURLToPath, URL } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@consulting19/shared': path.resolve(__dirname, '../../packages/shared/src'),
+      '@consulting19/shared': resolve(__dirname, '../../packages/shared/src'),
     },
   },
   optimizeDeps: {
