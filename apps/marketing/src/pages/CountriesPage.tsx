034da0deb9e5f7e438cf68ad3fd15c40133d5750
way-19 <asparossystem@gmail.com>
2025-09-01 23:44:27 -0700

    Remove extra closing div tag

--- apps/marketing/src/pages/CountriesPage.tsx
+++ apps/marketing/src/pages/CountriesPage.tsx
@@ -394,7 +394,6 @@ const CountriesPage = () => {
                 </Button>
               </Link>
           </div>
-        </div>
       </section>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
