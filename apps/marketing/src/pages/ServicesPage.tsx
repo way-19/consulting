42d665fc6f4ff3a1b256ca71aa65bc9d5c2d9bf0
way-19 <asparossystem@gmail.com>
2025-09-01 23:27:08 -0700

    Fix Link component import error

--- apps/marketing/src/pages/ServicesPage.tsx
+++ apps/marketing/src/pages/ServicesPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Building2, Calculator, CreditCard, FileText, Shield, TrendingUp, Users, BarChart3 } from 'lucide-react';
 import { useLanguage } from '../lib/language';
 import { Card, Button } from '../lib/ui';
