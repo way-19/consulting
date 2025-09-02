8462f23e005fd0ea63e089a695bb88e7145dbe9b
way-19 <asparossystem@gmail.com>
2025-09-02 01:32:25 -0700

    Updated main.tsx

--- apps/consultant/src/main.tsx
+++ apps/consultant/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import './i18n';
+import './i18n.ts';
 import App from './App.tsx';
 import './index.css';
 
