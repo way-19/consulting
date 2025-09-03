c02c283a1e5a93eb80b0a5f5dfcaefe67f56c042
way-19 <asparossystem@gmail.com>
2025-09-03 08:24:39 -0700

    Updated vite.config.ts

--- apps/marketing/vite.config.ts
+++ apps/marketing/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  root: path.resolve(__dirname, './'), // Bu satırı ekleyin
   plugins: [react()],
   resolve: {
     alias: {
