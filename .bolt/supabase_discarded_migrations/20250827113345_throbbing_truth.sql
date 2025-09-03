9259c8487d56c03fd07f959c631df7f1eeb6a8b2
way-19 <asparossystem@gmail.com>
2025-09-03 11:32:15 -0700

    Deleted 20250827113345_throbbing_truth.sql

--- .bolt/supabase_discarded_migrations/20250827113345_throbbing_truth.sql
+++
@@ -1,8 +0,0 @@
-@@ .. @@
- -- Update Georgia country to assign consultant
- UPDATE countries 
- SET consultant_id = '226c80f3-e1c3-416b-8289-e2929942b2e1',
--    featured = true,
--    updated_at = now()
-+    featured = true
- WHERE code = 'GE';
