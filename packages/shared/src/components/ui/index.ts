0c73dd070dbe05659c05a92ac458402a7098ac7a
way-19 <asparossystem@gmail.com>
2025-09-07 05:16:30 -0700

    Updated index.ts

--- packages/shared/src/components/ui/index.ts
+++ packages/shared/src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { Card } from './Card';
 export { MfaSetup } from './MfaSetup';
 export { MfaVerification } from './MfaVerification';
 export { AIAgentIcon } from './AIAgentIcon';
+export { NotificationBell } from './NotificationBell';
