d38d95e401fa582f4fe6c907bf3502576f126f04
way-19 <asparossystem@gmail.com>
2025-09-08 09:45:42 -0700

    Remove service page imports and routes from App.tsx

--- apps/marketing/src/App.tsx
+++ apps/marketing/src/App.tsx
@@ -1,18 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import { LanguageProvider } from './lib/language';
-import { AuthProvider } from '@consulting19/shared';
-import HomePage from './pages/HomePage';
-import CountriesPage from './pages/CountriesPage';
-import ServicesPage from './pages/ServicesPage';
-import ConsultantProfilePage from './pages/ConsultantProfilePage';
-import AuthPage from './pages/AuthPage';
-import BlogPage from './pages/BlogPage';
-import BlogPostPage from './pages/BlogPostPage';
-import ContactPage from './pages/ContactPage';
-import CountryPage from './pages/CountryPage';
-import AICountryRecommendationsPage from './pages/AICountryRecommendationsPage';
 import ComingSoonCountryPage from './pages/ComingSoonCountryPage';
 import AIExperiencePage from './pages/AIExperiencePage';
 import PrivacyPage from './pages/PrivacyPage';
@@ -31,24 +17,6 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/services" element={<ServicesPage />} />
-              <Route path="/countries" element={<CountriesPage />} />
-              <Route path="/countries/:countryCode" element={<CountryPage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/blog/:slug" element={<BlogPostPage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/blog/:slug" element={<BlogPostPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/ai-recommendations" element={<AICountryRecommendationsPage />} />
-              <Route path="/ai-experience" element={<AIExperiencePage />} />
-              <Route path="/privacy" element={<PrivacyPage />} />
-              <Route path="/terms" element={<TermsPage />} />
-              <Route path="/cookies" element={<CookiePolicyPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/sitemap" element={<SitemapPage />} />
-              <Route path="/consultant/:consultantId" element={<ConsultantProfilePage />} />
-              <Route path="/auth" element={<AuthPage />} />
-              <Route path="/order-form" element={<CompanyFormationWizard />} />
-              
               {/* Coming Soon Country Pages */}
               <Route path="/countries/usa" element={<ComingSoonCountryPage country="United States" flag="🇺🇸" />} />
               <Route path="/countries/uae" element={<ComingSoonCountryPage country="United Arab Emirates" flag="🇦🇪" />} />
