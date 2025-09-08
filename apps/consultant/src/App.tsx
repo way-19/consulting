246cadb3448d4c64d10cdbd2637832ed9256d098
way-19 <asparossystem@gmail.com>
2025-09-08 09:47:57 -0700

    Implement consultant dashboard core features

--- apps/consultant/src/App.tsx
+++ apps/consultant/src/App.tsx
@@ -9,6 +9,8 @@ import ConsultantDocuments from './pages/consultant/ConsultantDocuments';
 import ConsultantAvailability from './pages/consultant/ConsultantAvailability';
 import ConsultantServices from './pages/consultant/ConsultantServices';
 import ConsultantContent from './pages/consultant/ConsultantContent';
+import ConsultantMessages from './pages/consultant/ConsultantMessages';
+import ConsultantSettings from './pages/consultant/ConsultantSettings';
 import ConsultantLayout from './components/layouts/ConsultantLayout';
 
 function App() {
@@ -49,16 +51,20 @@ const ProtectedConsultantRoutes = () => {
   }
   
   return (
-    <Routes>
-      <Route path="/" element={<ConsultantDashboard />} />
-      <Route path="/clients" element={<ConsultantClients />} />
-      <Route path="/tasks" element={<ConsultantTasks />} />
-      <Route path="/documents" element={<ConsultantDocuments />} />
-      <Route path="/availability" element={<ConsultantAvailability />} />
-      <Route path="/services" element={<ConsultantServices />} />
-      <Route path="/content" element={<ConsultantContent />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <ConsultantLayout>
+      <Routes>
+        <Route path="/" element={<ConsultantDashboard />} />
+        <Route path="/clients" element={<ConsultantClients />} />
+        <Route path="/tasks" element={<ConsultantTasks />} />
+        <Route path="/documents" element={<ConsultantDocuments />} />
+        <Route path="/availability" element={<ConsultantAvailability />} />
+        <Route path="/services" element={<ConsultantServices />} />
+        <Route path="/content" element={<ConsultantContent />} />
+        <Route path="/messages" element={<ConsultantMessages />} />
+        <Route path="/settings" element={<ConsultantSettings />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </ConsultantLayout>
   );
 };
 
