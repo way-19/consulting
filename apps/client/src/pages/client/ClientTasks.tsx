4a1973e10cccd0f782e641ceab7dc447e5d5408e
way-19 <asparossystem@gmail.com>
2025-09-08 01:07:55 -0700

    Fix Link import error in ClientTasks

--- apps/client/src/pages/client/ClientTasks.tsx
+++ apps/client/src/pages/client/ClientTasks.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 import { useAuth, useI18n } from '@consulting19/shared';
 import { 
   CheckSquare, 
