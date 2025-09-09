1b573ef8c27383195278767f9e685cedd70bbf60
way-19 <asparossystem@gmail.com>
2025-09-09 07:13:44 -0700

    Updated ConsultantClients.tsx

--- apps/consultant/src/pages/consultant/ConsultantClients.tsx
+++ apps/consultant/src/pages/consultant/ConsultantClients.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useAuth } from '@consulting19/shared';
 import { 
-import { 
   Users, 
   Plus, 
   Search,
